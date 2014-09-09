5ea9652e5a57199ed3769fd56d56396d25a177c1
Supriyo Chakraborty <supriyo.chakraborty@gmail.com>
2014-09-09 04:14:58 -0400

    Changes to access Acc, Gyro, and Light sensors. The data accessed are written to the log.

--- src/edu/ucla/ee/nesl/MainActivity.java
+++ src/edu/ucla/ee/nesl/MainActivity.java
@@ -1,22 +1,37 @@
 package edu.ucla.ee.nesl;
 
+
 import android.os.Bundle;
 import android.app.Activity;
+
+import android.content.Context;
 import android.content.Intent;
+import android.text.Editable;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
-	Button startButton, stopButton;
-	
+ public class MainActivity extends Activity {
 	String TAG = "MainActivity";
-    @Override
+	Context mContext;
+	Button startButton, stopButton;
+	EditText editTextClientID, editTextServer, editTextPort;
+	String client = "", server = "";
+	int port = 0;
+	Boolean isAccChecked = false, isGpsChecked = false, isGyroChecked = false, isLightChecked = false;
+	
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mContext = this.getApplicationContext();
+        
         setContentView(R.layout.activity_main);
+        
         startButton = (Button)findViewById(R.id.startButton);
         startButton.setOnClickListener(startListener); 
         
@@ -24,24 +39,76 @@ public class MainActivity extends Activity {
         stopButton.setOnClickListener(stopListener); 
         
     }
-
-    @Override
+	
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-  
-    //Create an anonymous implementation of OnClickListener
+	
+	void readParamValues() {
+		Editable tmp;
+		CheckBox tempCheck;
+		
+		// read the client, server and port values. 
+		editTextClientID = (EditText)findViewById(R.id.clientID);
+		tmp = (Editable)editTextClientID.getText();
+        client = tmp.toString().trim();
+        
+        editTextServer = (EditText)findViewById(R.id.server);
+        tmp = (Editable)editTextServer.getText();
+        server = tmp.toString().trim();
+        
+        editTextPort = (EditText)findViewById(R.id.port);
+        tmp = (Editable)editTextPort.getText();
+        port = Integer.parseInt(tmp.toString().trim());
+        
+        tempCheck = (CheckBox)findViewById(R.id.acc);
+        isAccChecked = tempCheck.isChecked();
+        
+        tempCheck = (CheckBox)findViewById(R.id.gps);
+        isGpsChecked = tempCheck.isChecked();
+        
+        tempCheck = (CheckBox)findViewById(R.id.gyro);
+        isGyroChecked = tempCheck.isChecked();
+        
+        tempCheck = (CheckBox)findViewById(R.id.light);
+        isLightChecked = tempCheck.isChecked();
+	}
+	
+	public Intent getIntent() {
+		Intent mIntent = new Intent(MainActivity.this, dataService.class);
+		mIntent.putExtra("isAccChecked", isAccChecked);
+		mIntent.putExtra("isGpsChecked", isGpsChecked);
+		mIntent.putExtra("isGyroChecked", isGyroChecked);
+		mIntent.putExtra("isLightChecked", isLightChecked);
+		
+		mIntent.putExtra("client", client);
+		mIntent.putExtra("server", server);
+		mIntent.putExtra("port", port);
+		return mIntent;
+	}
+	
+	//Create an anonymous implementation of OnClickListener
     private OnClickListener startListener = new OnClickListener() {
+    	Intent mIntent;
 		@Override
 		public void onClick(View v) {
 			Log.d(TAG,"onClick() called - start button");
-			Intent intent = new Intent(MainActivity.this, dataService.class);
-			startService(intent);
-			startButton.setEnabled(false);
-			stopButton.setEnabled(true);
-			
+			readParamValues();	
+			if(!client.isEmpty() && !server.isEmpty() && port != 0) {	
+				mIntent = getIntent();
+				startService(mIntent);
+				startButton.setEnabled(false);
+				stopButton.setEnabled(true);
+			}
+			else {
+				String errStr = "You need to provide Client, Server and Port details to connect";
+				Toast.makeText(mContext, errStr, Toast.LENGTH_LONG).show();
+				Log.d(TAG, errStr);
+			}
+			Log.d(TAG, "Client = " + client + "; Server = " + server + "; Port = " + port);
 		}                                                                                          
     };  
 
@@ -55,6 +122,4 @@ public class MainActivity extends Activity {
             stopService(intent);
         } 
     };
-
-    
-}
+ }
--- src/edu/ucla/ee/nesl/dataService.java
+++ src/edu/ucla/ee/nesl/dataService.java
@@ -2,12 +2,24 @@ package edu.ucla.ee.nesl;
 
 import android.app.Service;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
 
-public class dataService extends Service {
+public class dataService extends Service implements SensorEventListener, LocationListener {
 
 	String TAG = "DataService" ;
+	SensorManager sensorManager = null;
+	LocationManager locationManager = null;
+	String server = "", client = "";
+	int port = 0;
 	
 	@Override
 	public IBinder onBind(Intent arg0) {
@@ -19,18 +31,80 @@ public class dataService extends Service {
 	public void onCreate() {
 		// TODO Auto-generated method stub
 		Log.d("TAG", "Creating Service First Time");
+		sensorManager = (SensorManager) this.getSystemService(this.SENSOR_SERVICE);
+		locationManager = (LocationManager)this.getSystemService(this.LOCATION_SERVICE);
 		super.onCreate();
 	}
 
 	@Override
 	public void onDestroy() {
 		Log.d(TAG, "Stopping Service");
+		sensorManager.unregisterListener(this);
 		super.onDestroy();
 	}
 
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
+		Sensor sensor;
 		Log.d("TAG", "Starting Service");
+		// Log.d(TAG,"server = " + intent.getStringExtra("server") + " client = " + intent.getStringExtra("client") + " port = " + intent.getIntExtra("port",0));
+		server = intent.getStringExtra("server");
+		client = intent.getStringExtra("client");
+		port = intent.getIntExtra("port",0);
+		//connect = connectToMQTT(server, client, port);
+		if(intent.getBooleanExtra("isAccChecked", false)) {
+			sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+			sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+		}
+		if(intent.getBooleanExtra("isGyroChecked", false)) {
+			sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+			sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+		}
+		if(intent.getBooleanExtra("isLightChecked", false)) {
+			sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+			sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+		}
 		return super.onStartCommand(intent, flags, startId);
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor arg0, int arg1) {
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+			Log.d(TAG, "a-X = " + event.values[0] + "; a-Y = " + event.values[1] + "; a-Z = " + event.values[2]);
+		}
+		if(event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+			Log.d(TAG, "g-X = " + event.values[0] + "; g-Y = " + event.values[1] + "; g-Z = " + event.values[2]);
+		}
+		if(event.sensor.getType() == Sensor.TYPE_LIGHT) {
+			Log.d(TAG, "light = " + event.values[0]);
+		}
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+		
 	}
 }
